3e962fb59975057041eee3dc830c7dd7ad6a87f6
Prashanth Radhakrishnan <shanth@google.com>
2023-10-25 12:53:54 -0700

    Fix url scraper.

--- tools/charts/README.md
+++ tools/charts/README.md
@@ -8,7 +8,7 @@ To generate `var_existence.csv`, run:
 SELECT DISTINCT O.observation_about AS Place, V.dcid AS Var
 FROM `datcom-store.scratch.ChartVars` AS V
 JOIN `datcom-store.scratch.ChartPlaces` AS P ON TRUE
-JOIN `datcom-store.scratch.StatVarObservation` AS O ON TRUE
+JOIN `datcom-store.dc_kg_latest.StatVarObservation` AS O ON TRUE
 WHERE
    O.variable_measured = V.dcid AND
    O.observation_about = P.dcid
--- tools/charts/gen.py
+++ tools/charts/gen.py
@@ -12,20 +12,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import csv
+from absl import app
+from absl import flags
 import glob
 import json
 from multiprocessing import current_process
 from multiprocessing import Pool
 import os
+import sys
 import time
 from typing import List
 
 import requests
 
+_IN_PATTERN = flags.DEFINE_string('in_pattern', 'output/urls/shard_*.txt',
+                                  'Input directory')
+_OUT_DIR = flags.DEFINE_string('out_dir', 'output/charts',
+                               'Output directory')
+_INCLUDE_URL = flags.DEFINE_bool('include_url_in_result',
+                                 False, '')
+_MIN_SKIP = flags.DEFINE_integer('min_to_skip', 30, '')
+
+
 _NPROC = 30
-_IN_PATTERN = 'output/urls/shard_*.txt'
-_OUT_DIR = 'output/charts'
 
 
 def _lines(fpath: str, count_only: bool) -> List[str]:
@@ -46,14 +55,14 @@ def gen(fpath: str):
   urls, _ = _lines(fpath, count_only=False)
   name = os.path.basename(fpath)
   name = name.replace('.txt', '.jsonl')
-  newf = os.path.join(_OUT_DIR, name)
+  newf = os.path.join(_OUT_DIR.value, name)
 
   # Load from prior checkpoint
   idx = 0
   if os.path.isfile(newf):
     _, idx = _lines(newf, count_only=True)
 
-  if len(urls) - idx <= 30:
+  if len(urls) - idx <= _MIN_SKIP.value:
     # Consider this good enough.
     print(f'Already full')
     return
@@ -68,7 +77,14 @@ def gen(fpath: str):
           print(f'Got empty for {urls[idx]}')
           idx += 1
           continue
-        jsons.append(resp)
+        if _INCLUDE_URL.value:
+          out_json = {
+            'url': urls[idx],
+            'result': resp
+          }
+        else:
+          out_json = resp
+        jsons.append(out_json)
       except Exception as e:
         print(f'Exception accessing {urls[idx]}: {e}')
         idx += 1
@@ -79,22 +95,30 @@ def gen(fpath: str):
         end = time.time()
         print(f'{current_process().name}: checkpointing {end - start}')
         for j in jsons:
-          fp.write(f'{j}\n')
+          fp.write(f'{json.dumps(j)}\n')
         jsons = []
         start = end
 
       idx += 1
 
+    # Final write
+    for j in jsons:
+      fp.write(f'{json.dumps(j)}\n')
 
-def main():
+
+def parse_flags():
+  flags.FLAGS(sys.argv)
+
+
+def main(_):
+  os.makedirs(_OUT_DIR.value, exist_ok=True)
   files = []
-  for f in glob.glob(_IN_PATTERN):
+  for f in glob.glob(_IN_PATTERN.value):
     files.append(f)
 
-  pool = Pool(_NPROC)
+  pool = Pool(_NPROC, initializer=parse_flags)
   pool.map(gen, files)
 
 
 if __name__ == "__main__":
-  os.makedirs(_OUT_DIR, exist_ok=True)
-  main()
+  app.run(main)
